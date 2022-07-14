d402def0c1f3f50045981c75ee738ae3faacca52
Kavin-kumar10 <kavinkumarprof@gmail.com>
2022-07-14 13:58:12 +0530

    professional commit

---
+++ help.js
@@ -0,0 +1,7 @@
+const cursor = document.getElementById("cursor");
+document.addEventListener("mousemove",function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+})
